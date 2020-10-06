b8ad3eefa788077ccb941267c30817d14507dbad
yaoMYZ <mozhenyaomyz@163.com>
2020-10-06 15:08:24 +0800

    添加 834

---
+++ LeetCode/834.py
@@ -0,0 +1,37 @@
+class Solution:
+    def sumOfDistancesInTree(self, N, edges):
+        tree = [[] for _ in range(N)]
+        for baba, erza in edges:
+            tree[baba].append(erza)
+            tree[erza].append(baba)
+        depth = [0 for _ in range(N)]
+        count = [0 for _ in range(N)]
+
+        def dfsForDepthAndCount(baba0, father):
+            count[baba0] = 1
+            for erza0 in tree[baba0]:
+                if erza0 != father:
+                    depth[erza0] = depth[baba0] + 1
+                    dfsForDepthAndCount(erza0, baba0)
+                    count[baba0] += count[erza0]
+
+        dfsForDepthAndCount(0, -1)
+        answer = [0 for _ in range(N)]
+        answer[0] = sum(depth)
+
+        def dfsForAnswer(baba1, father):
+            for erza1 in tree[baba1]:
+                if erza1 != father:
+                    answer[erza1] = answer[baba1] + N - 2 * count[erza1]
+                    dfsForAnswer(erza1, baba1)
+
+
+        dfsForAnswer(0, -1)
+        return answer
+
+
+if __name__ == '__main__':
+    so = Solution()
+    N = 3
+    edges = [[2,0],[1,0]]
+    print(so.sumOfDistancesInTree(N,edges))
