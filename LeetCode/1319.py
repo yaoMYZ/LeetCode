11d2a316bbc26a303539bd55f58d4276e997f9c3
yaoMYZ <mozhenyaoMYZ@163.com>
2021-01-23 14:28:32 +0800

    修改1319

--- LeetCode/1319.py
+++ LeetCode/1319.py
@@ -28,12 +28,6 @@ class UnionFind(object):
         self.sets_count -= 1                    # 连通后集合总数减一
 
 
-    def find_and_union(self,p):
-        for q in self.connect_sets:
-            if p!=q:
-                self.union(p,q)
-                break
-
 class Solution(object):
     def makeConnected(self, n, connections):
         """
@@ -44,28 +38,13 @@ class Solution(object):
         if len(connections)<n-1:
             return -1
         union = UnionFind(n)
-        repeat_cons = []
 
         for con in connections:
             p1 = union.find(con[0])
             p2 = union.find(con[1])
             if p1!=p2:
                 union.union(p1,p2)
-            else:
-                repeat_cons.append(con)
-        if union.sets_count<=1:
-            return 0
-        result = 0
-        for con in repeat_cons:
-            p = union.find(con[0])
-            union.find_and_union(p)
-            result+=1
-            if union.sets_count <= 1:
-                break
-        if union.sets_count<=1:
-            return result
-        else:
-            return -1
+        return union.sets_count - 1
 
 if __name__ == '__main__':
     so = Solution()
